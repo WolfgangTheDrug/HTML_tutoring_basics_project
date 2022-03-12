f7878a6335755efc3cb6fe9cc2d9a5e18523d4c2
WolfgangTheDrug <bardak_widz@wp.pl>
2022-03-12 13:37:47 +0100

    js is still buggy

--- Project 1 - Simple Calculator/script.js
+++ Project 1 - Simple Calculator/script.js
@@ -1,29 +1,42 @@
-const numberButtons = document.querySelectorAll('[data-number]')
+const numberButtons = document.querySelectorAll('[data-number]') /*const = variable*/
+//console.log(numberButtons);
+
 const operationsButtons = document.querySelectorAll('[data-operation]')
+//console.log(operationsButtons)
+
 const equalsButton = document.querySelector('[data-equals]')
+//console.log(equalsButton)
+
 const deleteButton = document.querySelector('[data-delete]')
+//console.log(deleteButton)
+
 const allClearButton = document.querySelector('[data-allClear]')
+//console.log(allClearButton)
+
 const plusMinusButton = document.querySelector('[data-plusMinus]')
+//console.log(plusMinusButton)
+
 const previousOperand = document.querySelector('[data-previousOperand]')
+//console.log(previousOperand)
+
 const currentOperand = document.querySelector('[data-currentOperand]')
+//console.log(currentOperand)
+
 const allButtons = document.querySelectorAll('button')
+//console.log(allButtons)
 
 class Calculator {
-    #previousOperandText;
-    #currentOperandText = '';
     constructor(previousOperandText,currentOperandText) {
-        this.#previousOperandText = previousOperandText;
-        this.#currentOperandText = currentOperandText;
+        this.previousOperandText = previousOperandText;
+        this.currentOperandText = currentOperandText;
     };
-    get currentOperandText() {
-        return this.#currentOperandText
-    }
     appendNumber(numb) {
-        const curr = currentOperand.innerHTML
+        const curr = this.currentOperandText
         if (numb === '.' && curr.indexOf('.') >= 0) {
             numb = ''
         }
-        currentOperand.innerHTML += numb;
+        this.currentOperandText += numb;
+        this.updateDisplay;
     }
     changeSign() {
         console.log('The sign has been changed')
@@ -41,13 +54,14 @@ class Calculator {
         console.log('Total')
     }
     updateDisplay() {
-        console.log(this.currentOperandText)
+        previousOperand.innerHTML = this.previousOperandText;
+        currentOperand.innerHTML = this.currentOperandText;
     }
 }
 
-const calculator = new Calculator('x','y')
+const calculator = new Calculator('','0')
 numberButtons.forEach(element =>
-    element.addEventListener('click', () => calculator.appendNumber(element.innerHTML))
+    element.addEventListener('click', () => calculator.appendNumber (element.innerHTML))
 )
 operationsButtons.forEach(element =>
     element.addEventListener('click', calculator.operate)
@@ -56,10 +70,12 @@ equalsButton.addEventListener('click', calculator.equals)
 deleteButton.addEventListener('click', calculator.delete)
 allClearButton.addEventListener('click', calculator.clearAll)
 plusMinusButton.addEventListener('click', calculator.changeSign)
+
 allButtons.forEach(element =>
     element.addEventListener('click', calculator.updateDisplay)
 )
 
-numberButtons.forEach(element =>
-    element.addEventListener('click', () => console.log(element.innerHTML))
-)
+
+// numberButtons.forEach(element =>
+//     element.addEventListener('click', () => console.log(element.innerHTML))
+// )
