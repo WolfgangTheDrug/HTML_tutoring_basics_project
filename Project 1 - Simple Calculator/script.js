c66e4d5200b7abc7a84d9d5bf8924b108ea15a1e
WolfgangTheDrug <bardak_widz@wp.pl>
2022-03-13 13:50:38 +0100

    Resignation from OOP approach

--- Project 1 - Simple Calculator/script.js
+++ Project 1 - Simple Calculator/script.js
@@ -1,81 +1,26 @@
-const numberButtons = document.querySelectorAll('[data-number]') /*const = variable*/
-//console.log(numberButtons);
-
+const numberButtons = document.querySelectorAll('[data-number]')
 const operationsButtons = document.querySelectorAll('[data-operation]')
-//console.log(operationsButtons)
-
 const equalsButton = document.querySelector('[data-equals]')
-//console.log(equalsButton)
-
-const deleteButton = document.querySelector('[data-delete]')
-//console.log(deleteButton)
-
+const removeCharacterButton = document.querySelector('[data-removeCharacter]')
 const allClearButton = document.querySelector('[data-allClear]')
-//console.log(allClearButton)
-
 const plusMinusButton = document.querySelector('[data-plusMinus]')
-//console.log(plusMinusButton)
-
 const previousOperand = document.querySelector('[data-previousOperand]')
-//console.log(previousOperand)
-
 const currentOperand = document.querySelector('[data-currentOperand]')
-//console.log(currentOperand)
-
 const allButtons = document.querySelectorAll('button')
-//console.log(allButtons)
 
-class Calculator {
-    constructor(previousOperandText,currentOperandText) {
-        this.previousOperandText = previousOperandText;
-        this.currentOperandText = currentOperandText;
-    };
-    appendNumber(numb) {
-        const curr = this.currentOperandText
-        if (numb === '.' && curr.indexOf('.') >= 0) {
-            numb = ''
-        }
-        this.currentOperandText += numb;
-        this.updateDisplay;
-    }
-    changeSign() {
-        console.log('The sign has been changed')
-    }
-    clearAll() {
-        console.log('Everything has been cleared')
-    }
-    delete() {
-        console.log('The last digit has been deleted')
-    }
-    operate() {
-        console.log('Operation has been changed')
-    }
-    equals() {
-        console.log('Total')
-    }
-    updateDisplay() {
-        previousOperand.innerHTML = this.previousOperandText;
-        currentOperand.innerHTML = this.currentOperandText;
-    }
+const appendNumber = function(numb) { currentOperand.innerHTML += (numb === '.' && currentOperand.innerHTML.indexOf('.') >= 0 ? '' : numb); }
+const changeSign = function() { currentOperand.innerHTML *= -1; }
+const clearAll = function() {
+    previousOperand.innerHTML = '';
+    currentOperand.innerHTML = '0';
 }
+const removeCharacter = function() { currentOperand.innerHTML = currentOperand.innerHTML.slice(0, -1); } // requires editing
+const operate = function() { console.log('Operation has been changed'); }
+const equals = function() { console.log('Total'); }
 
-const calculator = new Calculator('','0')
-numberButtons.forEach(element =>
-    element.addEventListener('click', () => calculator.appendNumber (element.innerHTML))
-)
-operationsButtons.forEach(element =>
-    element.addEventListener('click', calculator.operate)
-)
-equalsButton.addEventListener('click', calculator.equals)
-deleteButton.addEventListener('click', calculator.delete)
-allClearButton.addEventListener('click', calculator.clearAll)
-plusMinusButton.addEventListener('click', calculator.changeSign)
-
-allButtons.forEach(element =>
-    element.addEventListener('click', calculator.updateDisplay)
-)
-
-
-// numberButtons.forEach(element =>
-//     element.addEventListener('click', () => console.log(element.innerHTML))
-// )
+numberButtons.forEach( element => element.addEventListener('click', () => appendNumber (element.innerHTML)) )
+operationsButtons.forEach( element => element.addEventListener('click', operate) )
+equalsButton.addEventListener('click', equals)
+removeCharacterButton.addEventListener('click', removeCharacter)
+allClearButton.addEventListener('click', clearAll)
+plusMinusButton.addEventListener('click', changeSign)
