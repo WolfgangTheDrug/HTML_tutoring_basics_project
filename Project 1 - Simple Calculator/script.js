50daade58039d5743bae214ab8a8578fa61f3c27
WolfgangTheDrug <bardak_widz@wp.pl>
2022-04-03 00:48:43 +0200

    One operation problem solved, new ones emerge on the horizon!

--- Project 1 - Simple Calculator/script.js
+++ Project 1 - Simple Calculator/script.js
@@ -7,6 +7,7 @@ const plusMinusButton = document.querySelector('[data-plusMinus]')
 const previousOperand = document.querySelector('[data-previousOperand]')
 const currentOperand = document.querySelector('[data-currentOperand]')
 const allButtons = document.querySelectorAll('button')
+let previousOperation = '' // !
 
 const appendNumber = function(numb) {
   if(currentOperand.innerHTML === '0' && numb !== '.'){
@@ -19,14 +20,15 @@ const appendNumber = function(numb) {
 }
 const changeSign = function() { currentOperand.innerHTML *= -1; }
 const clearAll = function() {
-    previousOperand.innerHTML = '';
-    currentOperand.innerHTML = '0';
+    previousOperand.innerHTML = ''
+    currentOperand.innerHTML = '0'
+    previousOperation = '' // !
 }
 const removeCharacter = function() { currentOperand.innerHTML = currentOperand.innerHTML.slice(0, -1); } // requires editing
 const operate = function(operation) {
   const num1 = previousOperand.innerHTML === '' ? 0 : parseFloat(previousOperand.innerHTML)
   const num2 = currentOperand.innerHTML === '' ? 0 : parseFloat(currentOperand.innerHTML)
-  switch (operation) {
+  switch (previousOperation) {
     case '+':
       previousOperand.innerHTML = num1 + num2
       break;
@@ -39,9 +41,13 @@ const operate = function(operation) {
     case '÷':
       previousOperand.innerHTML = num1 / num2
       break;
+    default:
+      previousOperand.innerHTML = currentOperand.innerHTML // !
+      break;
   }
   previousOperand.innerHTML += operation
   currentOperand.innerHTML = '0'
+  previousOperation = operation // !
 }
 const equals = function() {
   const operations = ['+', '-', '×', '÷']
@@ -50,8 +56,6 @@ const equals = function() {
       const num = previousOperand.innerHTML.slice(0,-1)
       operate(op)
       currentOperand.innerHTML = previousOperand.innerHTML.slice(0, -1)
-    } else {
-      currentOperand.innerHTML = previousOperand.innerHTML
     }
     previousOperand.innerHTML = ''
 }
